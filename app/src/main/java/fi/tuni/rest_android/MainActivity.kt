7805a51944d39b0d2eee753516ae32737d95209b
ottokyosti <otto.t.kyosti@tuni.fi>
2023-05-08 20:17:31 +0300

    feat: User interface improvements

--- app/src/main/java/fi/tuni/rest_android/MainActivity.kt
+++ app/src/main/java/fi/tuni/rest_android/MainActivity.kt
@@ -3,17 +3,29 @@ package fi.tuni.rest_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.tuni.rest_android.ui.theme.RestAndroidTheme
 import kotlin.concurrent.thread
@@ -35,13 +47,59 @@ class MainActivity : ComponentActivity() {
                         ) {
                             LazyColumn {
                                 items(usersList!!) { user ->
-                                    Text(user.toString())
+                                    UserCard(user)
                                 }
                             }
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun UserCard(user : User) {
+    val context = LocalContext.current
+    Row (
+         modifier = Modifier
+             .fillMaxWidth()
+             .padding(10.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context)
+                .data(user.image)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp, 50.dp)
+                .clip(CircleShape)
+                .border(1.dp, MaterialTheme.colors.primary, CircleShape)
+                .padding(10.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        UserInfo(user)
+    }
+}
+
+@Composable
+fun UserInfo(user : User) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        color = MaterialTheme.colors.background,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 5.dp
+    ) {
+        Column(
+            Modifier.padding(5.dp)
+        ) {
+            Text(
+                text = "${user.firstName} ${user.lastName}",
+                fontWeight = FontWeight.Bold
+            )
+            Text("Age: ${user.age.toString()}")
         }
     }
 }
--- app/src/main/java/fi/tuni/rest_android/User.kt
+++ app/src/main/java/fi/tuni/rest_android/User.kt
@@ -3,11 +3,12 @@ package fi.tuni.rest_android
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class User (var id : Int? = null,
-                 var firstName : String? = null,
-                 var lastName : String? = null) {
+data class User (var firstName : String? = null,
+                 var lastName : String? = null,
+                 var age : Int? = null,
+                 var image : String? = null) {
 
     override fun toString() : String {
-        return "Id: $id, name: $firstName $lastName"
+        return "name: $firstName $lastName"
     }
 }
