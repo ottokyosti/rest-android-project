755f7eb77cc00e5e0b3924d2c989457672ff73e0
ottokyosti <otto.t.kyosti@tuni.fi>
2023-05-13 15:02:11 +0300

    feat: ui buttons for delete, edit and expand info

--- app/src/main/java/fi/tuni/rest_android/MainActivity.kt
+++ app/src/main/java/fi/tuni/rest_android/MainActivity.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,11 +24,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.tuni.rest_android.ui.theme.RestAndroidTheme
-import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
     private val client = Models()
@@ -40,7 +40,7 @@ class MainActivity : ComponentActivity() {
             runOnUiThread {
                 setContent {
                     RestAndroidTheme {
-                        var usersList by remember { mutableStateOf(users) }
+                        val usersList by remember { mutableStateOf(users) }
                         Surface(
                             modifier = Modifier.fillMaxSize(),
                             color = MaterialTheme.colors.background
@@ -86,20 +86,105 @@ fun UserCard(user : User) {
 
 @Composable
 fun UserInfo(user : User) {
+    var isExpanded by remember { mutableStateOf(false) }
+    var openDeleteConfirm by remember { mutableStateOf(false) }
     Surface(
         modifier = Modifier.fillMaxWidth(),
         color = MaterialTheme.colors.background,
         shape = RoundedCornerShape(8.dp),
         elevation = 5.dp
     ) {
-        Column(
-            Modifier.padding(5.dp)
-        ) {
-            Text(
-                text = "${user.firstName} ${user.lastName}",
-                fontWeight = FontWeight.Bold
-            )
-            Text("Age: ${user.age.toString()}")
+        Box {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(5.dp)
+            ) {
+                Text(
+                    text = user.toString(),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 2
+                )
+            }
+            Column(
+                Modifier.align(Alignment.TopEnd)
+            ) {
+                if (isExpanded) {
+                    IconButton(
+                        onClick = { openDeleteConfirm = !openDeleteConfirm }
+                    ) {
+                        Icon(
+                            Icons.Filled.Delete,
+                            contentDescription = "Delete user"
+                        )
+                    }
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Edit user"
+                        )
+                    }
+                }
+                IconButton(
+                    onClick = { isExpanded = !isExpanded }
+                ) {
+                    Icon(
+                        if (isExpanded) {
+                            Icons.Filled.ExpandLess
+                        } else {
+                            Icons.Filled.ExpandMore
+                        },
+                        contentDescription = "Expand user information"
+                    )
+                }
+            }
+        }
+        if (openDeleteConfirm) {
+            MyDeleteConfirmDialog {
+                openDeleteConfirm = !openDeleteConfirm
+            }
         }
     }
+}
+
+@Composable
+fun MyDeleteConfirmDialog(callback: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = { callback() },
+        title = {
+            Text(
+                text = "Do you want to delete this user?",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+        },
+        text = {
+            Text(
+                text = "Please confirm your decision",
+            )
+        },
+        dismissButton = {
+            OutlinedButton(
+                onClick = { callback() },
+                border = BorderStroke(0.dp, Color.Transparent)
+            ) {
+                Text(
+                    text = "Cancel",
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        },
+        confirmButton = {
+            OutlinedButton(
+                onClick = { callback() },
+                border = BorderStroke(0.dp, Color.Transparent)
+            ) {
+                Text(
+                    text = "Confirm",
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    )
 }
--- app/src/main/java/fi/tuni/rest_android/User.kt
+++ app/src/main/java/fi/tuni/rest_android/User.kt
@@ -5,10 +5,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class User (var firstName : String? = null,
                  var lastName : String? = null,
+                 var username : String? = null,
                  var age : Int? = null,
+                 var gender : String? = null,
+                 var email : String? = null,
+                 var phone : String? = null,
+                 var height : String? = null,
+                 var weight : String? = null,
                  var image : String? = null) {
 
     override fun toString() : String {
-        return "name: $firstName $lastName"
+        return "${this.firstName} ${this.lastName}\n" +
+                "Username: ${this.username}\n" +
+                "Age: ${this.age}\n" +
+                "Gender: ${this.gender}\n" +
+                "Email: ${this.email}\n" +
+                "Phone: ${this.phone}\n" +
+                "Height: ${this.height} cm\n" +
+                "Weight: ${this.weight} kg"
     }
 }
